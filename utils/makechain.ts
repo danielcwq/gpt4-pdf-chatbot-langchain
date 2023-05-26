6482c0929b6df41df874f4a7b3c0ffcdbd5dd4d4
Daniel Ching <dancwq2@gmail.com>
2023-05-26 10:44:19 +0800

    Added changes, hopefully changing from gpt4 to gpt3.5 will reduce rendering time

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -21,8 +21,8 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 10, // increase temepreature to get more creative answers
-    modelName: 'gpt-4', //change this to gpt-4 if you have access
+    temperature: 0.1, // increase temepreature to get more creative answers
+    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
